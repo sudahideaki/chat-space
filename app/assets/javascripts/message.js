576af8f374748004f240e9321b742e2d34ec4cb2
須田英明 <suda@sudahideakinoMacBook-Pro.local>
2019-11-10 18:52:31 +0900

    コメント　消去

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,12 +53,12 @@ $(function(){
     last_message_id = $('.message:last').data("message-id");
     
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      
       url: 'api/messages',
-      //ルーティングで設定した通りhttpメソッドをgetに指定
+      
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
+      
       data: {id: last_message_id}
     })
 
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,4 @@
 json.(@message, :content, :image)
 json.date @message.created_at.strftime('%Y/%m/%d %H:%M:%S')
 json.user_name @message.user.name
-#idもデータとして渡す
 json.id @message.id
