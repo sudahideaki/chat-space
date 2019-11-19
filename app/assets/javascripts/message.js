8bcfdb43757da601c67fb86fda3fce70d64288d7
須田英明 <suda@sudahideakinoMacBook-Pro.local>
2019-11-19 19:51:16 +0900

    最新のメッセージの時のみ自動スクロールする。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,34 +50,27 @@ $(function(){
 
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
     
-    last_message_id = $('.message:last').data("message-id");
-    
-    $.ajax({
-      
-      url: 'api/messages',
-      
-      type: 'get',
-      dataType: 'json',
-      
-      data: {id: last_message_id}
-    })
+      last_message_id = $('.message:last').data("message-id");
+  
+      $.ajax({
+        url: 'api/messages',
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id},
+      })
 
-    .done(function(messages) {
-      var insertHTML = '';
-      messages.forEach(function (message) {
-      var insertHTML = buildHTML(message);
-      $('.messages').append(insertHTML);
-    })
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-    })
-    .fail(function() {
-      alert('自動更新に失敗しました');
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+        var insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
     });
-  
-  
-
-  
-  };
-}
-setInterval(reloadMessages, 5000);
+   };
+  }
+  setInterval(reloadMessages, 5000);
 });
