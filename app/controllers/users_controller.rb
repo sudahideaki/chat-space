17d221f334f5a76a5b8d885cfaa07ac674340ba9
須田英明 <suda@sudahideakinoMacBook-Pro.local>
2019-11-21 18:22:34 +0900

    既存ユーザーを招待 可能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    return nil if params[:keyword] == ""
+    # return nil if params[:keyword] == ""
     @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
     respond_to do |format|
       format.html
